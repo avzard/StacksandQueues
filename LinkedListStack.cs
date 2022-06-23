a4d61898df863f1830e0e47656f91d1186722257
Abhishek Jadhav <59304331+avzard@users.noreply.github.com>
2022-06-23 10:14:46 +0530

    Add files via upload

--- LinkedListStack.cs
+++ LinkedListStack.cs
@@ -26,6 +26,33 @@ namespace StacksandQueues
             top = node;
 
         }
+        internal void peek()
+        {
+            if (top == null)
+            {
+                Console.WriteLine("Stack is Empty...");
+            }
+            Console.WriteLine("\nTop Most Element in Stack : " + top.data);
+        }
+
+        internal void pop()
+        {
+            if (top == null)
+            {
+                Console.WriteLine("Stack is Empty... ");
+            }
+            Console.WriteLine("Deleted Element : " + top.data);
+            top = top.next;
+        }
+        internal void isEmpty()
+        {
+            while (top != null)
+            {
+                peek();
+                pop();
+            }
+            Console.WriteLine("Stack is Empty...");
+        }
 
         public void Display()
         {
